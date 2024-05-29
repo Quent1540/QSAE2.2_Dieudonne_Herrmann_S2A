915aa567ec4fac330edf7681dc797687aab95e85
Quent1540 <dieudonnequent1@gmail.com>
2024-05-29 10:03:49 +0200

    commit 7

---
+++ src/main/java/org/example/fichiers/BellmanFord.java
@@ -0,0 +1,28 @@
+package org.example.fichiers;
+
+public class BellmanFord {
+    public Valeur resoudre(Graphe g, String depart){
+        for
+        //Pour chaque noeud X de G faire
+        //L(X) ←+∞
+        //parent(X) ←nul
+         //       FinPour
+
+        //L(depart) ←0
+
+        //Répéter
+        //modifie ←faux
+
+        //Pour chaque noeud X de G faire
+        //Pour chaque N adjacent à X faire
+        //Si L (X) + coût(X, N) < L(N) alors
+        //L(N) ←L(X) + coût(X, N)
+        //parent(N) ←X
+        //modifie ←vrai
+        //        FinSi
+        //FinPour
+        //        FinPour
+        //Jusqu 'à ce que modifie = faux
+        //Fin
+    }
+}
--- src/test/java/TestGraphe.java
+++ src/test/java/TestGraphe.java
@@ -11,22 +11,14 @@ public class TestGraphe {
     public void testConstructionGraphe() {
         GrapheListe g = new GrapheListe();
 
-        // Ajoutez des arcs au graphe
         g.ajouterArc("A", "B", 10);
         g.ajouterArc("A", "C", 20);
         g.ajouterArc("B", "D", 15);
 
-        // Vérifiez si les arcs ont été correctement ajoutés
-        assertEquals(0, g.getIndice("A")); // Vérifie que le nœud "A" a été ajouté
-        assertEquals(1, g.getIndice("B")); // Vérifie que le nœud "B" a été ajouté
-        assertEquals(2, g.getIndice("C")); // Vérifie que le nœud "C" a été ajouté
-        assertEquals(3, g.getIndice("D")); // Vérifie que le nœud "D" a été ajouté
+        assertEquals(0, g.getIndice("A")); //On verifie que le noeud A a bien ete ajoute
+        assertEquals(3, g.getIndice("D")); // On verifie que le noeud D a bien ete ajoute
 
-        // Vérifiez les arcs sortants du nœud "A"
-        assertEquals(2, g.getAdjacence().get(0).getArcs().size()); //Il doit y avoir deux arcs sortant de A
         assertEquals("B", g.getAdjacence().get(0).getArcs().get(0).getDest()); //Le premier arc sortant de A doit etre B
         assertEquals(12, g.getAdjacence().get(0).getArcs().get(0).getCout()); // Le cout du premier arc sortant de A doit etre egal au cout de l'arc reliant A a B, soit 12
     }
-
-    // Ajoutez d'autres méthodes de test selon vos besoins
 }
